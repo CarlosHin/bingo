cf8d90c0d3b5fc1344eb092ed704423b3bad1692
Carlos <carloshin98@gmail.com>
2022-12-24 13:45:46 +0100

    bingo commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '../styles/globals.css'
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from '@chakra-ui/react'
+
+const config = {
+  initialColorMode: 'dark'
+}
+const theme = extendTheme({ config })
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
 }
